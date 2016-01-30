4a6a0c8f2c525b08735f5fd52feab5a5da404fc9
Ryan Riley <ryan@tachyus.com>
2016-01-30 12:14:08 -0600

    Use Suave from a console application

---
+++ slides/Program.fs
@@ -0,0 +1,18 @@
+﻿module Program
+
+open Freya.Core.Integration
+open Suave
+open Suave.Logging
+open Suave.Owin
+
+[<EntryPoint>]
+let main args =
+    let serverConfig = 
+        { defaultConfig with
+           bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" 7000 ]
+           logger = Loggers.saneDefaultsFor LogLevel.Verbose
+        }
+    let app =
+        OwinApp.ofAppFunc "/" (OwinAppFunc.ofFreya Api.app)
+    startWebServer serverConfig app
+    0
